c9a95a04dc66a11150f11e2c80ebc0e9614807f3
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2020-05-23 18:00:16 +0000

    Add AutoFocus

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,30 +1,25 @@
-import React, {Component} from 'react';
-import '../App.css';
+import React, { Component } from "react";
+import "../App.css";
 
-export default class Search extends Component{
-onSubmit(e){
+export default class Search extends Component {
+  onSubmit(e) {
     e.preventDefault();
-let username = this.refs.username.value.trim();
-if(!username){
-    alert("Please type in a username")
-    return;
-}
-this.props.onFormSubmit(username);
+    let username = this.refs.username.value.trim();
+    if (!username) {
+      alert("Please type in a username");
+      return;
+    }
+    this.props.onFormSubmit(username);
 
-this.refs.username.value =' ';
-}
-
-    render = () => 
-    <div>
-<form onSubmit={this.onSubmit.bind(this)}>
-    <label>Search Github Users</label>
-    <input type="text" ref="username" className="form-control" />
-</form>
-  
-  </div>
-  
-  
-  
-  
+    this.refs.username.value = " ";
   }
-  
+
+  render = () => (
+    <div>
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <label>Search Github Users</label>
+        <input autoFocus type="text" ref="username" className="form-control" />
+      </form>
+    </div>
+  );
+}
